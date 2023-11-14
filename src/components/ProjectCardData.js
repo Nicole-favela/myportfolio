9a6903b7e77cb0551f367bd9270f461b65b92f53
Nicole Favela <nicolefavela@Nicoles-MacBook-Pro-2.local>
2023-11-14 14:56:41 -0700

    added netflix clone project

--- src/components/ProjectCardData.js
+++ src/components/ProjectCardData.js
@@ -5,6 +5,7 @@ import finalyrproj from "../assets/final_yr_proj_map.png"
 import financeproj from "../assets/finance_tracker_proj.png"
 import tmpimg from "../assets/headphonespic.png"
 import spotify_clone_img1 from "../assets/spotify-proj-img1.png"
+import netflix_proj_p1 from "../assets/netflix_clone_p1.png"
 const ProjectData = [
     {
         imgsrc: proj1,
@@ -33,6 +34,13 @@ const ProjectData = [
         text: "Listen to music, view playlists, music library, even search by song/artist. Built using React, Node, spotify API, spotify-web-api-node, and MUI components.",
         view: "https://github.com/Nicole-favela/SpotifyClone",
         site: "https://www.dropbox.com/scl/fi/61yxjbnc5uqddot3z29jp/spotify_clone_demo.MP4?rlkey=svn3wpsaw2afvumwf3sx9nmb2&dl=0"
+    },
+    {
+        imgsrc: netflix_proj_p1,
+        title: "Full stack Netflix clone",
+        text: "This Netflix clone app was built with the MERN stack and incorporates user authentication using JWT and cookies for a seamless login and registration experience. Leveraging the TMDb API, the application mirrors Netflix's layout and design while utilizing React Player to stream movie trailers. The platform keeps track of users' recently viewed trailers and allows them to curate a personalized 'My List' with the ability to add or remove movies. Additionally, users can access their account details and log out through a dedicated profile page.",
+        view: "https://github.com/Nicole-favela/Netflix-clone",
+        site: " ",
     },
     {
         imgsrc: tmpimg,
